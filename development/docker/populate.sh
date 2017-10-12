2ead575987eb18eb8218e3adff9ae6cef0dfd782
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2017-10-12 09:40:09 +0200

    feat: new complete data
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- development/docker/populate.sh
+++ development/docker/populate.sh
@@ -6,6 +6,6 @@ invenio db init create
 invenio index init
 invenio index queue init
 #invenio records create --pid-minter recid  reroils/src/reroils-app/development/docker/demo.json
-dojson -i reroils/src/reroils-data/data/10k_2017_10_06.json schema http://ils.test.rero.ch/schema/records/record-v0.0.1.json | invenio records create --pid-minter bibid
+dojson -i reroils/src/reroils-data/data/10k_2017_10_06_complete.json schema http://ils.test.rero.ch/schema/records/record-v0.0.1.json | invenio records create --pid-minter bibid
 invenio index reindex --yes-i-know --pid-type recid
 invenio index run
