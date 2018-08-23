0bb5769a4ca6b1ec25779058096238cca03b4c52
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2018-08-23 11:22:32 +0200

    fix: populate for deployement
    Signed-off-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- deployment/populate.sh
+++ deployment/populate.sh
@@ -59,4 +59,4 @@ invenio fixtures createitems -R
 invenio fixtures createcirctransactions circulation_transactions.json 
 
 # put OAI configuration
-invenio oaiharvester initconfig $VIRTUAL_ENV/src/reroils-data/data/oaisources.yml
+invenio oaiharvester initconfig oaisources.yml
