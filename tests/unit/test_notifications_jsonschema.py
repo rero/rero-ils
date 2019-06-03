fb24a56ba3ec3ae074f1e390312f71fcd3d44cf6
Aly Badr <aly.badr@rero.ch>
2019-06-03 08:46:23 +0200

    notifications: create resource data model and api
    * Adds notification schema, mappings and required objects
    * Integrate invenio-records 1.2.1 to have a separate table by resource
    
    Signed-off-by: Aly Badr <aly.badr@rero.ch>

---
+++ tests/unit/test_notifications_jsonschema.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of RERO ILS.
+# Copyright (C) 2018 RERO.
+#
+# RERO ILS is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# RERO ILS is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RERO ILS; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, RERO does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Notifications JSON schema tests."""
+
+from __future__ import absolute_import, print_function
+
+import pytest
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+
+def test_required(notification_schema, dummy_notification):
+    """Test required for notification jsonschemas."""
+    validate(dummy_notification, notification_schema)
+
+    with pytest.raises(ValidationError):
+        validate({}, notification_schema)
+        validate(dummy_notification, notification_schema)
+
+
+def test_pid(notification_schema, dummy_notification):
+    """Test pid for notification jsonschemas."""
+    validate(dummy_notification, notification_schema)
+
+    with pytest.raises(ValidationError):
+        dummy_notification['pid'] = 25
+        validate(dummy_notification, notification_schema)
+
+
+def test_notification_type(
+        notification_schema, dummy_notification):
+    """Test type for notification jsonschemas."""
+    validate(dummy_notification, notification_schema)
+
+    with pytest.raises(ValidationError):
+        dummy_notification['notification_type'] = 25
+        validate(dummy_notification, notification_schema)
+
+
+def test_loan(notification_schema, dummy_notification):
+    """Test loan for notification jsonschemas."""
+    validate(dummy_notification, notification_schema)
+
+    with pytest.raises(ValidationError):
+        dummy_notification['loan'] = 25
+        validate(dummy_notification, notification_schema)
