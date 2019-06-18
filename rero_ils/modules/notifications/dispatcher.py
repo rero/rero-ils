81aaee990e2f2f684ae48d19e6ed03036c47eeba
Aly Badr <aly.badr@rero.ch>
2019-06-18 11:01:06 +0200

    notification templates: create notification templates
    * Adds notification templates by type, language and communication channel.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

--- rero_ils/modules/notifications/dispatcher.py
+++ rero_ils/modules/notifications/dispatcher.py
@@ -26,6 +26,7 @@
 
 from __future__ import absolute_import, print_function
 
+from flask import current_app
 from flask_security.utils import config_value
 from invenio_mail.api import TemplatedMessage
 from invenio_mail.tasks import send_email
@@ -38,15 +39,26 @@ class Dispatcher():
         """Dispatch the notification."""
         if notification:
             data = notification.replace_pids_and_refs()
-            self.send_mail(data=data)
+            communication_channel = \
+                data['loan']['patron']['communication_channel']
+            if communication_channel == 'email':
+                self.send_mail(data=data)
+            if communication_channel == 'sms':
+                pass
+            if communication_channel == 'whatsapp':
+                pass
+            if communication_channel == 'letter':
+                pass
             notification = notification.update_process_date()
         return notification
 
     def send_mail(self, data):
         """Send email."""
+        data['loan']['profile_url'] = \
+            'https://ils.test.rero.ch/patrons/profile'
         notification_type = data.get('notification_type')
-        language = 'eng'
-        template = 'email/{type}_{lang}'.format(
+        language = data['loan']['patron']['communication_language']
+        template = 'email/{type}/{lang}'.format(
             type=notification_type,
             lang=language
         )
@@ -54,13 +66,17 @@ class Dispatcher():
         msg = TemplatedMessage(
             # template_html='{template}.html'.format(template=template),
             template_body='{template}.txt'.format(template=template),
-            subject=notification_type,
             sender=config_value('EMAIL_SENDER'),
             recipients=[recipient],
             ctx=data['loan']
         )
-
+        text = msg.body.split('\n')
+        msg.subject = text[0]
+        msg.body = '\n'.join(text[1:])
         try:
-            send_email.delay(msg.__dict__)
+            send_email.run(msg.__dict__)
+            # TODO: investigate why delay does not work
+            # send_email.delay(msg.__dict__)
+            # current_app.extensions['mail'].send(msg)
         except Exception as e:
             raise(e)
