764849d09b01a66e717d253d4f85e6d163d74ec0
Igor Milhit <igor.milhit@rero.ch>
2019-07-04 10:13:43 +0200

    license: move from GPLv2 to AGPLv3
    * NEW Changes the LICENSE file content.
    * NEW Changes the files headers.
    * Orders authors alphabetically.
    * Restore Simonetta in data/loans.json.
    
    Co-Authored-by: Igor Milhit <igor.milhit@rero.ch>

--- tests/api/test_organisations_rest_api.py
+++ tests/api/test_organisations_rest_api.py
@@ -1,15 +1,19 @@
-# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# -*- coding: utf-8 -*-
 #
-# You should have received a copy of the GNU General Public License
-# along with RERO ILS; if not, write to the
-# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-# MA 02111-1307, USA.
+# RERO ILS
+# Copyright (C) 2019 RERO
 #
-# In applying this license, RERO does not
-# waive the privileges and immunities granted to it by virtue of its status
-# as an Intergovernmental Organization or submit itself to any jurisdiction.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Tests REST API organisations."""
 
--- tests/ui/test_indexer_utils.py
+++ tests/ui/test_indexer_utils.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2019 RERO
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -9,7 +13,7 @@
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """API tests for indexer utilities."""
 
