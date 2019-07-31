5d017ded1bc4ef0b15d3ee908ec7479c8592a616
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2019-07-31 17:31:05 +0200

    editor: compact the presentation
    * Improves editor appearance.
    * Improves js package manager: use yarn instead of npm.
    * Save vertical space by putting add item in menu
    
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

---
+++ ui/src/app/records/editor/bootstrap4-framework/custombootstrap4-framework.spec.ts
@@ -0,0 +1,7 @@
+import { CustomBootstrap4Framework } from './custombootstrap4-framework';
+
+describe('CustomBootstrap4Framework', () => {
+  it('should create an instance', () => {
+    expect(new CustomBootstrap4Framework()).toBeTruthy();
+  });
+});
