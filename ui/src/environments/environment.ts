951b5aa157980a4bfc0746cbf42d8c874d2b86a7
Igor Milhit <igor.milhit@rero.ch>
2019-08-07 14:18:23 +0200

    license: move from GPLv2 to AGPLv3
    * UI angular application.
    
    Co-Authored-by: Igor Milhit <igor.milhit@rero.ch>

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,3 +1,22 @@
+/*
+
+RERO ILS
+Copyright (C) 2019 RERO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
