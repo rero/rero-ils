95bb93d1b6a3e5bf21eda9664803a4827879124c
Aly Badr <aly.badr@rero.ch>
2019-09-04 17:31:10 +0200

    holdings: calculate record availability
    * Adds holding availability.
    * Provides availability loan conditions for holdings records.
    
    Co-Authored-by: Laurent Dubois <laurent.dubois@uclouvain.be>
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

--- rero_ils/modules/holdings/api_views.py
+++ rero_ils/modules/holdings/api_views.py
@@ -20,10 +20,37 @@
 
 from __future__ import absolute_import, print_function
 
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify
+
+from .api import Holding
+from ..items.api_views import check_authentication, jsonify_error
 
 api_blueprint = Blueprint(
     'api_holding',
     __name__,
     url_prefix='/holding'
 )
+
+
+@api_blueprint.route('/availabilty/<holding_pid>', methods=['GET'])
+@check_authentication
+@jsonify_error
+def holding_availability(holding_pid):
+    """HTTP GET request for holding availability."""
+    holding = Holding.get_record_by_pid(holding_pid)
+    if not holding:
+        abort(404)
+    return jsonify({
+        'availability': holding.available
+    })
+
+
+@api_blueprint.app_template_filter()
+def holding_loan_condition(holding_pid):
+    """HTTP GET request for holding loan condition."""
+    holding = Holding.get_record_by_pid(holding_pid)
+    if not holding:
+        abort(404)
+    return jsonify({
+        'loan_condition': holding.get_holding_loan_conditions()
+    })
