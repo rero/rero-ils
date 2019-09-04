b1983cd5a9e80ee7a1183b130f85dbcdbdadeede
Aly Badr <aly.badr@rero.ch>
2019-09-04 17:31:10 +0200

    holdings: calculate record availability
    * Adds holding availability.
    * Provides availability loan conditions for holdings records.
    
    Co-Authored-by: Laurent Dubois <laurent.dubois@uclouvain.be>
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

--- tests/api/test_availability.py
+++ tests/api/test_availability.py
@@ -22,33 +22,37 @@ from flask import url_for
 from invenio_accounts.testutils import login_user_via_session
 from utils import get_json
 
+from rero_ils.modules.holdings.api import Holding
 from rero_ils.modules.items.api import Item
 from rero_ils.modules.items.views import not_available_reasons
 from rero_ils.modules.loans.api import LoanAction
 
 
-def test_item_availability(
-        client, item_lib_martigny, loc_public_saxon,
-        librarian_martigny_no_email, patron_martigny_no_email,
-        circulation_policies, librarian_saxon_no_email):
+def test_item_holding_availability(
+        client, holding_lib_martigny, item_lib_martigny, item2_lib_martigny,
+        librarian_martigny_no_email, librarian_saxon_no_email,
+        patron_martigny_no_email, loc_public_saxon, circulation_policies):
     """Test item availability."""
-    def available_status(pid, user):
-        """Returns item availability."""
-        login_user_via_session(client, user)
-        res = client.get(
-            url_for(
-                'api_item.item_availability',
-                item_pid=pid,
-            )
-        )
-        assert res.status_code == 200
-        data = get_json(res)
-        return data.get('availability')
 
-    assert available_status(
-        item_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert item_availablity_status(
+        client, item_lib_martigny.pid, librarian_martigny_no_email.user)
     assert item_lib_martigny.available
     assert not not_available_reasons(item_lib_martigny)
+    assert holding_lib_martigny.available
+    assert holding_availablity_status(
+        client, holding_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert holding_lib_martigny.get_holding_loan_conditions() == 'standard'
+
+    # login as patron
+    with mock.patch(
+        'rero_ils.modules.patrons.api.current_patron',
+        patron_martigny_no_email
+    ):
+        login_user_via_session(client, patron_martigny_no_email.user)
+        assert holding_lib_martigny.get_holding_loan_conditions() \
+            == 'short 15 days'
+
+    # request
     login_user_via_session(client, librarian_martigny_no_email.user)
 
     res = client.post(
@@ -67,9 +71,14 @@ def test_item_availability(
     actions = data.get('action_applied')
     loan_pid = actions[LoanAction.REQUEST].get('pid')
     assert not item_lib_martigny.available
-    assert not available_status(
-        item_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert not item_availablity_status(
+        client, item_lib_martigny.pid, librarian_martigny_no_email.user)
     assert not_available_reasons(item_lib_martigny) == '(1 requests)'
+    holding = Holding.get_record_by_pid(holding_lib_martigny.pid)
+    assert holding.available
+    assert holding_availablity_status(
+        client, holding_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert holding_lib_martigny.get_holding_loan_conditions() == 'standard'
 
     # validate request
     res = client.post(
@@ -84,11 +93,16 @@ def test_item_availability(
     )
     assert res.status_code == 200
     assert not item_lib_martigny.available
-    assert not available_status(
-        item_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert not item_availablity_status(
+        client, item_lib_martigny.pid, librarian_martigny_no_email.user)
     assert not item_lib_martigny.available
     item = Item.get_record_by_pid(item_lib_martigny.pid)
     assert not_available_reasons(item) == 'in transit (1 requests)'
+    holding = Holding.get_record_by_pid(holding_lib_martigny.pid)
+    assert holding.available
+    assert holding_availablity_status(
+        client, holding_lib_martigny.pid, librarian_martigny_no_email.user)
+    assert holding_lib_martigny.get_holding_loan_conditions() == 'standard'
 
     login_user_via_session(client, librarian_saxon_no_email.user)
     # receive
@@ -104,11 +118,16 @@ def test_item_availability(
     )
     assert res.status_code == 200
     assert not item_lib_martigny.available
-    assert not available_status(
-        item_lib_martigny.pid, librarian_saxon_no_email.user)
+    assert not item_availablity_status(
+        client, item_lib_martigny.pid, librarian_saxon_no_email.user)
     item = Item.get_record_by_pid(item_lib_martigny.pid)
     assert not item.available
     assert not_available_reasons(item) == '(1 requests)'
+    holding = Holding.get_record_by_pid(holding_lib_martigny.pid)
+    assert holding.available
+    assert holding_availablity_status(
+        client, holding_lib_martigny.pid, librarian_saxon_no_email.user)
+    assert holding_lib_martigny.get_holding_loan_conditions() == 'standard'
 
     # checkout
     res = client.post(
@@ -125,8 +144,13 @@ def test_item_availability(
 
     item = Item.get_record_by_pid(item_lib_martigny.pid)
     assert not item.available
-    assert not available_status(
-        item.pid, librarian_martigny_no_email.user)
+    assert not item_availablity_status(
+        client, item.pid, librarian_martigny_no_email.user)
+    holding = Holding.get_record_by_pid(holding_lib_martigny.pid)
+    assert holding.available
+    assert holding_availablity_status(
+        client, holding_lib_martigny.pid, librarian_saxon_no_email.user)
+    assert holding_lib_martigny.get_holding_loan_conditions() == 'standard'
 
     class current_i18n:
         class locale:
@@ -137,3 +161,31 @@ def test_item_availability(
     ):
         end_date = item.get_item_end_date()
         assert not_available_reasons(item) == 'due until ' + end_date
+
+
+def item_availablity_status(client, pid, user):
+    """Returns item availability."""
+    login_user_via_session(client, user)
+    res = client.get(
+        url_for(
+            'api_item.item_availability',
+            item_pid=pid,
+        )
+    )
+    assert res.status_code == 200
+    data = get_json(res)
+    return data.get('availability')
+
+
+def holding_availablity_status(client, pid, user):
+    """Returns holding availability."""
+    login_user_via_session(client, user)
+    res = client.get(
+        url_for(
+            'api_holding.holding_availability',
+            holding_pid=pid,
+        )
+    )
+    assert res.status_code == 200
+    data = get_json(res)
+    return data.get('availability')
