998198020cb0c47c35de59a55de651ee591e578c
Olivier DOSSMANN <git@dossmann.net>
2019-09-24 22:32:34 +0200

    ui: display git commit hash on frontpage
    Homepage displays current RERO-ILS version. Version as in "release".
    It's not so accurate.
    
    Development server needs to display current git commit hash
    instead.
    
    Uses INVENIO_RERO_ILS_APP_GIT_HASH environment variable for this.
    
    Co-Authored-by: Olivier DOSSMANN <git@dossmann.net>

--- rero_ils/config.py
+++ rero_ils/config.py
@@ -1015,7 +1015,10 @@ RERO_ILS_APP_BASE_URL = 'https://ils.rero.ch'
 RERO_ILS_PERMALINK_RERO_URL = 'http://data.rero.ch/01-{identifier}'
 RERO_ILS_PERMALINK_BNF_URL = 'http://catalogue.bnf.fr/ark:/12148/{identifier}'
 
-#: RERO_ILS MEF specificconfigurations.
+#: Git commit hash. If set, a link to github commit page is displayed on RERO-ILS frontpage.
+RERO_ILS_APP_GIT_HASH = None
+
+#: RERO_ILS MEF specific configurations.
 RERO_ILS_MEF_HOST = 'mef.rero.ch'
 RERO_ILS_MEF_URL = 'https://{host}/api/mef/'.format(host=RERO_ILS_MEF_HOST)
 RERO_ILS_MEF_RESULT_SIZE = 100
