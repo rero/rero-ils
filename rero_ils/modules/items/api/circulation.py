f16ade64ab161f0ac8eda0fe393d61fae1bf2f54
Olivier DOSSMANN <git@dossmann.net>
2020-04-09 17:54:24 +0200

    project: update Pipfile dependencies
    While upgrading to invenio 3.2, especially invenio-circulation, the
    invenio-circulation module import marshmallow with some functionnality
    only available on 3.0.0 version.
    In the past RERO-ils added a constraint on marshmallow.
    This commit deletes this old constraint.
    
    * deletes marshmallow constraints
    * adds ciso8601 dependency
    * fixes isort problems on last code
    
    Co-Authored-by: Olivier DOSSMANN <git@dossmann.net>

--- rero_ils/modules/items/api/circulation.py
+++ rero_ils/modules/items/api/circulation.py
@@ -22,12 +22,11 @@ from functools import wraps
 
 from flask import current_app
 from invenio_circulation.api import get_loan_for_item
-from invenio_circulation.search.api import search_by_patron_item_or_document
-
 from invenio_circulation.errors import MissingRequiredParameterError, \
     NoValidTransitionAvailableError
 from invenio_circulation.proxies import current_circulation
-from invenio_circulation.search.api import search_by_pid
+from invenio_circulation.search.api import search_by_patron_item_or_document, \
+    search_by_pid
 from invenio_i18n.ext import current_i18n
 from invenio_records_rest.utils import obj_or_import_string
 from invenio_search import current_search
