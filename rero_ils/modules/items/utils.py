8c991a1ef4a9d4b4f038f470b0b62dcc8fdb6883
Aly Badr <aly.badr@rero.ch>
2020-04-22 11:19:56 +0200

    circulation: adapt reroils
    Adapts reroils after the upgrade to invenio-circulation v1.0.0a21.
    
    The two transitions ItemOnLoanToItemReturned and ItemOnLoanToItemReturned are
    temporarily updated and inserted into reroils until a fix for the issue
    https://github.com/inveniosoftware/invenio-circulation/issues/127 is available.
    
    This PR expects the fix for the ITEM_AT_DESK problem in the current version
    of invenio-circulation.
    
    Some timezone units testing are disabled until a fix is given in a later PR.
    
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

---
+++ rero_ils/modules/items/utils.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2019 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Item utils."""
+
+
+def item_pid_to_object(item_pid):
+    """Build an item_pid object from a given item pid.
+
+    :param item_pid: the item pid value
+    :return: the item_pid object
+    :rtype: object
+    """
+    return {'value': item_pid, 'type': 'item'}
+
+
+def item_location_retriever(item_pid):
+    """Returns the item shelflocation pid for the given item_pid.
+
+    :param item_pid: the item_pid object
+    :type item_pid: object
+    :return: the location pid of the item
+    :rtype: str
+    """
+    from .api import Item
+    # TODO: for requests we probably need the transation_location_pid
+    #       to deal with multiple pickup locations for a library
+    item = Item.get_record_by_pid(item_pid.get('value'))
+    if item:
+        # TODO: this will be useful for the very specific rero use cases
+
+        # last_location = item.get_last_location()
+        # if last_location:
+        #     return last_location.pid
+        return item.get_owning_pickup_location_pid()
