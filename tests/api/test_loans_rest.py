43739aaee5e442968ea13385f9c7256ff54406f9
Olivier DOSSMANN <git@dossmann.net>
2020-05-23 10:10:10 +0200

    loans: use datetime for [start|end]_date fields
    Before using invenio-circulation 1.0.0a21, loan use datetime format for
    start_date and end_date fields.
    RERO-ils uses hour in datetime fields to check timezone changes.
    
    After invenio-circulation 1.0.0a21, start_date and end_date change to
    date field only (it loses the hour).
    
    This commit takes care of hour in start_date and end_date fields.
    
    * Changes loan start_date and end_date to datetime field (to keep
    hours/minutes)
    * Uses again tests on timezones
    
    Co-Authored-by: Olivier DOSSMANN <git@dossmann.net>

--- tests/api/test_loans_rest.py
+++ tests/api/test_loans_rest.py
@@ -202,13 +202,11 @@ def test_due_soon_loans(client, librarian_martigny_no_email,
     loan_date = ciso8601.parse_datetime(checkout_loan.get('end_date'))
 
     # as instance timezone is Europe/Zurich, it should be either 21 or 22
-    # TODO: check why this fails
-    # check_timezone_date(pytz.utc, loan_date, [21, 22])
+    check_timezone_date(pytz.utc, loan_date, [21, 22])
 
     # should be 14:59/15:59 in US/Pacific (because of daylight saving time)
-    # TODO: check why these fail
-    # check_timezone_date(pytz.timezone('US/Pacific'), loan_date, [14, 15])
-    # check_timezone_date(pytz.timezone('Europe/Amsterdam'), loan_date)
+    check_timezone_date(pytz.timezone('US/Pacific'), loan_date, [14, 15])
+    check_timezone_date(pytz.timezone('Europe/Amsterdam'), loan_date)
 
     # checkin the item to put it back to it's original state
     res, _ = postdata(
@@ -553,8 +551,7 @@ It should be the same date, even if timezone changed."
     assert loan_datetime.month == lib_datetime.month, fail_msg
     assert loan_datetime.day == lib_datetime.day, fail_msg
     # Loan date differs regarding timezone, and day of the year (GMT+1/2).
-    # TODO: check why this fails
-    # check_timezone_date(pytz.utc, loan_datetime, [21, 22])
+    check_timezone_date(pytz.utc, loan_datetime, [21, 22])
 
 
 def test_librarian_request_on_blocked_user(
