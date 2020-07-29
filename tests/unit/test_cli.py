0846e7dc7c8d7e5ac084e36eee5fc1c087cb9626
Peter Weber <peter.weber@rero.ch>
2020-07-29 22:28:29 +0200

    cli: fix marc21 to json transformation
    * Fixes the marc21json and validate cli functions to work properly with
      json schema files containing $refs.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2019 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Test cli."""
+
+from os.path import dirname, join
+
+from click.testing import CliRunner
+
+from rero_ils.modules.cli import check_validate
+
+
+def test_cli_validate(app, script_info):
+    """Test validate cli."""
+    runner = CliRunner()
+    file_name = join(dirname(__file__), '../data/documents.json')
+
+    res = runner.invoke(
+        check_validate,
+        [file_name, 'doc', '-v'],
+        obj=script_info
+    )
+    assert res.output.strip().split('\n') == [
+        'Testing json schema for file',
+        '\tTest record: 1',
+        '\tTest record: 2'
+    ]
