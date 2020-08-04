84ca6fafbe1e32c4273bb01902054860258cb67b
Aly Badr <aly.badr@rero.ch>
2020-08-04 09:13:25 +0200

    circulation: add units testing for circulation scenarios
    Ensures that all circulation scenarios listed in the file
    https://github.com/rero/rero-ils/blob/dev/doc/circulation/scenarios.md
    are tested in the backend.
    
    Fixes bug: a checkout was not possible for an item with two pending loans.
    
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

---
+++ tests/api/circulation/scenarios/test_scenario_a.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2019 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests circulation scenario A."""
+
+
+from invenio_accounts.testutils import login_user_via_session
+from utils import get_json, postdata
+
+from rero_ils.modules.items.models import ItemStatus
+from rero_ils.modules.loans.api import Loan, LoanAction, LoanState, \
+    get_last_transaction_loc_for_item, get_loans_by_patron_pid
+
+
+def test_circ_scenario_a(
+        client, librarian_martigny_no_email, lib_martigny,
+        patron_martigny_no_email, loc_public_martigny, item_lib_martigny,
+        circulation_policies):
+    """Test the first circulation scenario."""
+    # https://github.com/rero/rero-ils/blob/dev/doc/circulation/scenarios.md
+    # A request is made on on-shelf item, that has no requests, to be picked
+    # up at the owning library. Validated by the librarian. Picked up at same
+    # owning library. and returned on-time at the owning library
+    login_user_via_session(client, librarian_martigny_no_email.user)
+    circ_params = {
+            'item_pid': item_lib_martigny.pid,
+            'patron_pid': patron_martigny_no_email.pid,
+            'pickup_location_pid': loc_public_martigny.pid,
+            'transaction_library_pid': lib_martigny.pid,
+            'transaction_user_pid': librarian_martigny_no_email.pid
+    }
+    # ADD_REQUEST_1.1
+    res, data = postdata(
+        client, 'api_item.librarian_request', dict(circ_params))
+    assert res.status_code == 200
+    request_loan_pid = get_json(res)['action_applied']['request']['pid']
+    # VALIDATE_1.2
+    circ_params['pid'] = request_loan_pid
+    res, data = postdata(
+        client, 'api_item.validate_request', dict(circ_params))
+    assert res.status_code == 200
+    # CHECKOUT_2.1
+    res, data = postdata(
+        client, 'api_item.checkout', dict(circ_params))
+    assert res.status_code == 200
+    # CHECKIN_3.1.1
+    res, data = postdata(
+        client, 'api_item.checkin', dict(circ_params))
+    assert res.status_code == 200
+    assert item_lib_martigny.status == ItemStatus.ON_SHELF
