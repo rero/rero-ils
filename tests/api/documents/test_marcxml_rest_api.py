2365686d6c50a7661bfadf27ef070c1dd6d95c11
Aly Badr <aly.badr@rero.ch>
2020-08-19 08:04:23 +0200

    document: allow posting documents using OAuth token
    With this commit, a new role `document_importer` is created for users
    creating document via the document post API only.
    
    Currently, this role is assigned to one invenio user for ngscan tests.
    
    A new cli to create personal OAuth token is added to the utils.
    
    * Fixtures and tests updated accordingly.
    
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

--- tests/api/documents/test_marcxml_rest_api.py
+++ tests/api/documents/test_marcxml_rest_api.py
@@ -18,15 +18,26 @@
 """Tests POST REST API for MARC21 documents."""
 
 
-import mock
-from utils import VerifyRecordPermissionPatch, postdata
+from click.testing import CliRunner
+from utils import login_user_via_session, postdata
+
+from rero_ils.modules.cli import tokens_create
 
 
-@mock.patch('invenio_records_rest.views.verify_record_permission',
-            mock.MagicMock(return_value=VerifyRecordPermissionPatch))
 def test_marcxml_documents_create(
-        client, document_marcxml, documents_marcxml, rero_marcxml_header):
-    """Test post of marcxml document."""
+        client, document_marcxml, documents_marcxml, rero_marcxml_header,
+        librarian_martigny_no_email):
+    """Test post of marcxml document for logged users."""
+    res, data = postdata(
+        client,
+        'invenio_records_rest.doc_list',
+        document_marcxml,
+        headers=rero_marcxml_header,
+        force_data_as_json=False
+    )
+    assert res.status_code == 401
+
+    login_user_via_session(client, librarian_martigny_no_email.user)
     res, data = postdata(
         client,
         'invenio_records_rest.doc_list',
@@ -46,3 +57,27 @@ def test_marcxml_documents_create(
         force_data_as_json=False
     )
     assert res.status_code == 400
+
+
+def test_marcxml_documents_create_with_a_token(
+        app, client, document_marcxml, rero_marcxml_header,
+        librarian_martigny_no_email, script_info):
+    """Test post of marcxml document with an access token."""
+    runner = CliRunner()
+    res = runner.invoke(
+        tokens_create,
+        ['-n', 'test', '-u', librarian_martigny_no_email.get('email'),
+         '-t', 'my_token'],
+        obj=script_info
+    )
+    access_token = res.output.strip().split('\n')[0]
+    res, data = postdata(
+        client,
+        'invenio_records_rest.doc_list',
+        document_marcxml,
+        url_data={'access_token': access_token},
+        headers=rero_marcxml_header,
+        force_data_as_json=False
+    )
+    assert res.status_code == 201
+    assert data['metadata']['_draft']
