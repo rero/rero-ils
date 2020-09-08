1141dd01d63ab6740bc2cfc1a61a45b2085dde91
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2020-09-08 07:43:05 +0200

    user: enable user profile
    * Enable invenio-userprofiles.
    * Adds missing templates such as breadcrumbs.
    * Removes personal data in the patron profile.
    * Adds a RERO ID profile menu.
    * Enables invenio-admin.
    
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- rero_ils/config.py
+++ rero_ils/config.py
@@ -142,9 +142,7 @@ FOOTER_TEMPLATE = 'rero_ils/footer.html'
 #: Header base template.
 HEADER_TEMPLATE = 'rero_ils/header.html'
 #: Settings base template
-SETTINGS_TEMPLATE = 'invenio_theme/page_settings.html'
-#: Admin base template
-ADMIN_BASE_TEMPLATE = BASE_TEMPLATE
+SETTINGS_TEMPLATE = 'rero_ils/page_settings.html'
 
 # Miscellaneous variable around templates
 # =======================
@@ -170,7 +168,6 @@ SEARCH_UI_HEADER_TEMPLATE = 'rero_ils/search_header.html'
 REROILS_SEARCHBAR_TEMPLATE = 'templates/rero_ils/searchbar.html'
 RERO_ILS_EDITOR_TEMPLATE = 'rero_ils/editor.html'
 RERO_ILS_COLLECTIONS_TEMPLATE = 'rero_ils/collections.html'
-SECURITY_LOGIN_USER_TEMPLATE = 'rero_ils/login_user.html'
 
 # Theme configuration
 # ===================
@@ -237,7 +234,7 @@ ACCOUNTS_SESSION_REDIS_URL = 'redis://localhost:6379/1'
 #: client. Set to False, in case of doubt.
 ACCOUNTS_USERINFO_HEADERS = False
 # Disable User Profiles
-USERPROFILES = False
+USERPROFILES = True
 # make security blueprints available to the REST API
 ACCOUNTS_REGISTER_BLUEPRINT = True
 
@@ -2000,7 +1997,7 @@ RECORDS_JSON_SCHEMA = {
 # Login Configuration
 # ===================
 #: Allow password change by users.
-SECURITY_CHANGEABLE = False
+SECURITY_CHANGEABLE = True
 
 #: Allow user to confirm their email address.
 SECURITY_CONFIRMABLE = True
@@ -2239,7 +2236,6 @@ CIRCULATION_ACTIONS_VALIDATION = {
     ]
 }
 
-WEBPACKEXT_PROJECT = 'rero_ils.webpack:project'
 
 # WIKI
 # ====
