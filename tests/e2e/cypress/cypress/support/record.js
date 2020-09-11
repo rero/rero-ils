631491eb9eb19536be1399e1887660d0cfff100e
Alicia Zangger <alicia.zangger@rero.ch>
2020-09-11 11:32:50 +0200

    cypress: create circulation scenario B test
    * Creates circulation scenario B test.
    * Removes checkout-checkin test, as this is not the template test anymore.
    * Adds fixtures for the test.
    * Corrects or improve some commands.
    
    Co-Authored-by: Alicia Zangger <alicia.zangger@rero.ch>

--- tests/e2e/cypress/cypress/support/record.js
+++ tests/e2e/cypress/cypress/support/record.js
@@ -42,3 +42,45 @@ Cypress.Commands.add("deleteRecordFromDetailView", () => {
   cy.get('#detail-delete-button').click();
   cy.get('#modal-confirm-button').click();
 });
+
+Cypress.Commands.add("populateSimpleRecord", (document) => {
+  cy.fixture('documents').then(function (documents) {
+    this.documents = documents;
+  });
+  // Choose type
+  cy.get('ng-core-editor-select-with-sort-type.ng-star-inserted > #type').select(document.type);
+
+  // Enter title
+  cy.get('#title-0-mainTitle-0-value').type(document.title.mainTitle);
+
+  // Enter provision activity
+  cy.get('#provisionActivity-0-type').select(document.provisionActivity.type);
+  cy.get('#provisionActivity-0-startDate').type(document.provisionActivity.publicationDate1);
+  cy.get('#provisionActivity-0-statement-0-label-0-value').type(document.provisionActivity.statement.place);
+  cy.get('#provisionActivity-0-statement-1-label-0-value').type(document.provisionActivity.statement.agent);
+  cy.get('#provisionActivity-0-statement-2-label-0-value').type(document.provisionActivity.statement.date);
+
+  // Choose language
+  cy.get('#language-0-value').select(document.language1);
+
+  // Choose mode of issuance
+  // TODO find a way to use custom id for oneOf
+});
+
+Cypress.Commands.add("saveRecord", () => {
+  // Assert save button is active
+  cy.get('#editor-save-button').should('not.be.disabled');
+
+  // Save document (redirection to detail document view)
+  cy.get('#editor-save-button').click();
+});
+
+Cypress.Commands.add("checkDocumentEssentialFields", (document) => {
+  cy.fixture('documents').then(function (documents) {
+    this.documents = documents;
+  });
+  cy.get('#doc-language-0').should('contain', document.language1);
+  cy.get('#doc-issuance').should('contain', document.issuance.mainTypeCode + ' / materialUnit');
+  cy.get('#doc-title').should('contain', document.title.mainTitle);
+  cy.get('#doc-provision-activity-0').should('contain', document.provisionActivity.statement.place + ' : ' + document.provisionActivity.statement.agent + ', ' + document.provisionActivity.statement.date);
+});
