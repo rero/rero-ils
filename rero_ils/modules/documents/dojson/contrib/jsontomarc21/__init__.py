0a2f3ae94bd632d6a791331d27fbe4d3abc72cfc
Peter Weber <peter.weber@rero.ch>
2021-04-15 17:34:54 +0200

    SRU: convert JSON to MARC21
    * Adds dojson for JSON to MARC21 transformation.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

---
+++ rero_ils/modules/documents/dojson/contrib/jsontomarc21/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2021 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""RERO MARC21 to JSON."""
+
+from .model import to_marc21
+
+__all__ = ('to_marc21')
