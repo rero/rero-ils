a890f7d21c95e76f3e8a30d8c4ada79bd37e9329
Renaud Michotte <renaud.michotte@gmail.com>
2021-05-05 15:21:39 +0200

    template: allow to user `type` field into templates
    Closes rero/rero-ils#1776.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- tests/ui/templates/test_templates_api.py
+++ tests/ui/templates/test_templates_api.py
@@ -43,24 +43,24 @@ def test_template_es_mapping(es_clear, db,
     assert mapping == get_mapping(search.Meta.index)
 
 
-def test_template_create(db, es_clear, templ_doc_public_martigny_data,
+def test_template_create(db, es_clear, templ_item_public_martigny_data,
                          org_martigny, system_librarian_martigny):
     """Test template creation."""
-    templ_doc_public_martigny_data['toto'] = 'toto'
+    templ_item_public_martigny_data['toto'] = 'toto'
     with pytest.raises(ValidationError):
-        temp = Template.create(templ_doc_public_martigny_data, delete_pid=True)
+        Template.create(templ_item_public_martigny_data, delete_pid=True)
 
     db.session.rollback()
 
     next_pid = Template.provider.identifier.next()
-    del templ_doc_public_martigny_data['toto']
-    temp = Template.create(templ_doc_public_martigny_data, delete_pid=True)
+    del templ_item_public_martigny_data['toto']
+    temp = Template.create(templ_item_public_martigny_data, delete_pid=True)
     next_pid += 1
-    assert temp == templ_doc_public_martigny_data
+    assert temp == templ_item_public_martigny_data
     assert temp.get('pid') == str(next_pid)
 
     temp = Template.get_record_by_pid(str(next_pid))
-    assert temp == templ_doc_public_martigny_data
+    assert temp == templ_item_public_martigny_data
 
     fetched_pid = fetcher(temp.id, temp)
     assert fetched_pid.pid_value == str(next_pid)
