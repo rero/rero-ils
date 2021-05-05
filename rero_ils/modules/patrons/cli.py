a79fe99c8eb614738f5d4b2154c428b1e2323b8f
Peter Weber <Peter.Weber@rero.ch>
2021-05-05 08:18:31 +0200

    cli: fix several things
    * Replaces `.format`with f''.
    * Corrects verbose message for updated records.
    * Changes warning message in dojson for media type.
    * Adds communication channel validation to `users_validate` CLI.
    * Adds libraries check to `users_validate` CLI.
    * Adds `db.session.rollback()` in exception in `import_users` CLI.
    * Corrects false creation of user creation error file.
    * Corrects pid dependencies configuration files.
    * Adds birth day checking to users validation CLI.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

--- rero_ils/modules/patrons/cli.py
+++ rero_ils/modules/patrons/cli.py
@@ -116,7 +116,7 @@ def import_users(infile, append, verbose, password, lazy, dont_stop_on_error,
                                 .format(count=count, username=username),
                                 fg='yellow')
         except Exception as err:
-            error_records.append(data)
+            error_records.append(patron_data)
             click.secho(
                 '{count: <8} User create error: {err}'.format(
                     count=count,
@@ -130,6 +130,8 @@ def import_users(infile, append, verbose, password, lazy, dont_stop_on_error,
                 sys.exit(1)
             if debug:
                 traceback.print_exc()
+            db.session.rollback()
+
     if append:
         click.secho(f'Append fixtures new identifiers: {len(pids)}')
         identifier = Patron.provider.identifier
@@ -163,7 +165,8 @@ def import_users(infile, append, verbose, password, lazy, dont_stop_on_error,
 @with_appcontext
 def users_validate(jsonfile, verbose, debug):
     """Check users validation."""
-    click.secho('Validate user file', fg='green')
+    click.secho('Validate user file: ', fg='green', nl=False)
+    click.echo(f'{jsonfile.name}')
 
     path = current_jsonschemas.url_to_path(get_schema_for_resource('ptrn'))
     ptrn_schema = current_jsonschemas.get_schema(path=path)
@@ -187,9 +190,24 @@ def users_validate(jsonfile, verbose, debug):
     datas = read_json_record(jsonfile)
     for idx, data in enumerate(datas):
         if verbose:
-            click.echo(f'\tTest record: {idx}')
+            click.echo(f'\tTest record: {idx} pid: {data.get("pid")}')
         try:
             validate(data, schema)
+            patron = data.get('patron', {})
+            if patron and patron.get('communication_channel') == 'email'\
+               and data.get('email') is None \
+               and patron.get('additional_communication_email') is None:
+                raise ValidationError('At least one email should be defined '
+                                      'for an email communication channel.')
+            librarian_roles = [
+                Patron.ROLE_SYSTEM_LIBRARIAN, Patron.ROLE_LIBRARIAN]
+            if any(role in librarian_roles for role in data.get('roles', [])):
+                if not data.get('libraries'):
+                    raise ValidationError('Missing libraries')
+            birth_date = data.get('birth_date')
+            if birth_date[0] == '0':
+                raise ValidationError(f'Wrong birth date: {birth_date}')
+
         except ValidationError as err:
             click.secho(
                 f'Error validate in record: {idx} pid: {data.get("pid")}',
