e70fb8663a20935efad3f7fd827164c33d2ff458
Renaud Michotte <renaud.michotte@gmail.com>
2021-05-18 15:16:01 +0200

    account: add features about acquisition account.
    * Adds fields on the `acq_account` resources and update mapping.
    * Updates fixtures.
    * Adds API to transfer funds between two accounts.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

---
+++ rero_ils/modules/acq_accounts/listener.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2021 RERO
+# Copyright (C) 2021 UCLouvain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Signals connector for Acquisition account."""
+from .api import AcqAccount, AcqAccountsSearch
+
+
+def enrich_acq_account_data(sender, json=None, record=None, index=None,
+                            doc_type=None, arguments=None, **dummy_kwargs):
+    """Signal sent before a record is indexed.
+
+    :param json: The dumped record dictionary which can be modified.
+    :param record: The record being indexed.
+    :param index: The index in which the record will be indexed.
+    :param doc_type: The doc_type for the record.
+    """
+    if index.split('-')[0] == AcqAccountsSearch.Meta.index:
+        account = record
+        if not isinstance(record, AcqAccount):
+            account = AcqAccount.get_record_by_pid(record.get('pid'))
+
+        # compute the exceedance amounts
+        amount = account.get('allocated_amount', 0)
+        if amount:
+            if 'encumbrance_exceedance' in account:
+                rate = account.get('encumbrance_exceedance')
+                json['encumbrance_exceedance'] = dict(
+                    value=rate,
+                    amount=round(amount * rate) / 100
+                )
+            if 'expenditure_exceedance' in account:
+                rate = account.get('expenditure_exceedance')
+                json['expenditure_exceedance'] = dict(
+                    value=rate,
+                    amount=round(amount * rate) / 100
+                )
+        else:
+            json.pop('encumbrance_exceedance', None)
+            json.pop('expenditure_exceedance', None)
+
+        # encumbrance, expenditure and balance amounts
+        (self_amount, children_amount) = account.encumbrance_amount
+        json['encumbrance_amount'] = dict(
+            self=self_amount,
+            children=children_amount,
+            total=self_amount + children_amount
+        )
+        (self_amount, children_amount) = account.expenditure_amount
+        json['expenditure_amount'] = dict(
+            self=self_amount,
+            children=children_amount,
+            total=self_amount + children_amount
+        )
+        (self_amount, total_amount) = account.remaining_balance
+        json['remaining_balance'] = dict(
+            self=self_amount,
+            total=total_amount
+        )
+
+        # additional fields for ES
+        json['is_active'] = account.is_active
+        json['depth'] = account.depth
+        json['distribution'] = account.distribution
+        json['organisation'] = dict(pid=account.organisation_pid)
