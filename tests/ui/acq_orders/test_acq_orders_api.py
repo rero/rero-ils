920eb652383e7cbccc153adec38da057a1896ec8
Renaud Michotte <renaud.michotte@gmail.com>
2021-09-06 16:03:36 +0200

    acquisition: create acquisition resources dumpers
    * Creates dumper classes for acquisitions resources. These dumpers will
      be used for order notification generation.
    * Creates order notification preview endpoint.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- tests/ui/acq_orders/test_acq_orders_api.py
+++ tests/ui/acq_orders/test_acq_orders_api.py
@@ -17,19 +17,23 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Acquisition orders API tests."""
+
 from rero_ils.modules.acq_order_lines.models import AcqOrderLineStatus
-from rero_ils.modules.acq_orders.models import AcqOrderStatus
+from rero_ils.modules.acq_orders.models import AcqOrderNoteType, AcqOrderStatus
 
 
-def test_order_status(
+def test_order_properties(
     acq_order_fiction_martigny,
     acq_order_line_fiction_martigny,
-    acq_order_line2_fiction_martigny
+    acq_order_line2_fiction_martigny,
+    yesterday
 ):
-    """Test order status."""
+    """Test order properties."""
     acol1 = acq_order_line_fiction_martigny
     acol2 = acq_order_line2_fiction_martigny
     acor = acq_order_fiction_martigny
+
+    # STATUS ------------------------------------------------------------------
     assert acol1['status'] == acol2['status'] == AcqOrderLineStatus.APPROVED
     assert acor.status == AcqOrderStatus.PENDING
 
@@ -47,13 +51,29 @@ def test_order_status(
     acol1['status'] = AcqOrderLineStatus.APPROVED
     acol1.update(acol1, dbcommit=True, reindex=True)
 
-
-def test_order_get_order_lines(
-    acq_order_fiction_martigny,
-    acq_order_line_fiction_martigny,
-    acq_order_line2_fiction_martigny
-):
-    """Test order get_order_lines."""
-    acor = acq_order_fiction_martigny
+    # ORDER LINES -------------------------------------------------------------
     assert len(list(acor.get_order_lines())) == \
         acor.get_order_lines(count=True)
+
+    # ORDER DATE --------------------------------------------------------------
+    assert acor.order_date is None
+
+    acol2['status'] = AcqOrderLineStatus.ORDERED
+    acol2['order_date'] = yesterday.strftime('%Y-%m-%d')
+    acol2.update(acol2, dbcommit=True, reindex=True)
+    assert acor.order_date == yesterday.strftime('%Y-%m-%d')
+
+    # reset changes
+    acol2['status'] = AcqOrderLineStatus.APPROVED
+    del acol2['order_date']
+    acol2.update(acol2, dbcommit=True, reindex=True)
+
+    # ORDER NOTE --------------------------------------------------------------
+    note_content = 'test note content'
+    assert acor.get_note(AcqOrderNoteType.VENDOR) is None
+    acor.setdefault('notes', []).append({
+        'type': AcqOrderNoteType.VENDOR,
+        'content': note_content
+    })
+    assert acor.get_note(AcqOrderNoteType.VENDOR) == note_content
+    del acor['notes']
