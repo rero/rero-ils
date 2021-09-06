34d1ac520649fe452da976b989750f3e7d6e24d1
Renaud Michotte <renaud.michotte@gmail.com>
2021-09-06 16:03:36 +0200

    acquisition: create acquisition resources dumpers
    * Creates dumper classes for acquisitions resources. These dumpers will
      be used for order notification generation.
    * Creates order notification preview endpoint.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

---
+++ rero_ils/modules/acq_orders/dumpers.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2021 RERO
+# Copyright (C) 2021 UCLouvain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Acquisition order dumpers."""
+
+from invenio_records.dumpers import Dumper as InvenioRecordsDumper
+
+from rero_ils.modules.acq_order_lines.dumpers import \
+    AcqOrderLineNotificationDumper
+from rero_ils.modules.acq_orders.models import AcqOrderNoteType
+from rero_ils.modules.libraries.api import Library
+from rero_ils.modules.libraries.dumpers import \
+    LibraryAcquisitionNotificationDumper
+
+
+class AcqOrderNotificationDumper(InvenioRecordsDumper):
+    """Order dumper class for acquisition order notification."""
+
+    def dump(self, record, data):
+        """Dump an AcqOrder instance for acquisition order notification.
+
+        :param record: The record to dump.
+        :param data: The initial dump data passed in by ``record.dumps()``.
+        """
+        data.update({
+            'reference': record.get('reference'),
+            'order_date': record.order_date,
+            'note': record.get_note(AcqOrderNoteType.VENDOR),
+        })
+        library = Library.get_record_by_pid(record.library_pid)
+        data['library'] = library.dumps(
+            dumper=LibraryAcquisitionNotificationDumper())
+        data['order_lines'] = [
+            order_line.dumps(dumper=AcqOrderLineNotificationDumper())
+            for order_line in record.get_order_lines()
+        ]
+        data = {k: v for k, v in data.items() if v}
+        return data
