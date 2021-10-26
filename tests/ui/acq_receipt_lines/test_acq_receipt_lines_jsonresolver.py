cf4c7111f2bcf79d1b84e4b190d17d9aea875c95
Aly Badr <aly.badr@rero.ch>
2021-10-26 14:15:19 +0200

    acquisition: add receipt_line resource
    Co-Authored-by: Aly Badr <aly.badr@rero.ch>

---
+++ tests/ui/acq_receipt_lines/test_acq_receipt_lines_jsonresolver.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2021 RERO
+# Copyright (C) 2021 UCLouvain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Acq receipt line JSONResolver tests."""
+
+import pytest
+from invenio_records.api import Record
+from jsonref import JsonRefError
+
+from rero_ils.modules.utils import extracted_data_from_ref
+
+
+def test_acq_receipt_lines_jsonresolver(acq_receipt_line_1_fiction_martigny):
+    """Acquisition receipt lines resolver tests."""
+    data = {'$ref': 'https://bib.rero.ch/api/acq_receipt_lines/acrl1'}
+    rec = Record.create({'acq_receipt_line': data})
+    assert extracted_data_from_ref(rec.get('acq_receipt_line')) == 'acrl1'
+    # deleted record
+    acq_receipt_line_1_fiction_martigny.delete()
+    with pytest.raises(JsonRefError):
+        rec.replace_refs().dumps()
+
+    # non existing record
+    data = {'$ref': 'https://bib.rero.ch/api/acq_receipt_lines/n_e'}
+    rec = Record.create({'acq_receipt_line': data})
+    with pytest.raises(JsonRefError):
+        rec.replace_refs().dumps()
