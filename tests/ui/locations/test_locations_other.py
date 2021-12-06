4be2dbb4539aaa5fe6b5ba21dfe1acee7654811d
Peter Weber <Peter.Weber@rero.ch>
2021-12-06 10:14:59 +0100

    locations: fix temporary deletion
    * Adds temporary location to item links to me.
    * Closes #2578.
    
    Co-Authored-by: Peter Weber <peter.weber@rero.ch>

--- tests/ui/locations/test_locations_other.py
+++ tests/ui/locations/test_locations_other.py
@@ -21,6 +21,7 @@
    scope level fixtures.
 """
 
+from rero_ils.modules.items.api import ItemsSearch
 from rero_ils.modules.locations.api import Location
 
 
@@ -32,3 +33,22 @@ def test_location_get_all_pickup_locations(
 
     locations = Location.get_pickup_location_pids(patron_martigny.pid)
     assert set(locations) == {loc_public_martigny.pid}
+
+
+def test_location_get_links_to_me(
+        loc_public_martigny, loc_public_sion, item_lib_martigny):
+    """Test pickup locations retrieval."""
+
+    assert loc_public_martigny.get_links_to_me() == {'items': 1}
+    assert loc_public_martigny.get_links_to_me(get_pids=True) == {
+        'items': ['item1']}
+
+    assert loc_public_sion.get_links_to_me() == {}
+    item_lib_martigny['temporary_location'] = {
+        '$ref': f'https://bib.rero.ch/api/locations/{loc_public_sion.pid}'}
+    item_lib_martigny.update(data=item_lib_martigny, dbcommit=True,
+                             reindex=True)
+    ItemsSearch.flush_and_refresh()
+    assert loc_public_sion.get_links_to_me() == {'items': 1}
+    assert loc_public_sion.get_links_to_me(get_pids=True) == {
+        'items': ['item1']}
