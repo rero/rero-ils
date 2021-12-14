5df7aab19f3f9a419f2c98222aeeedbc2fad4893
Renaud Michotte <renaud.michotte@gmail.com>
2021-12-14 08:36:59 +0100

    acquisition: add VAT rate field for receipt line
    Adds a new field into each reception line to specify the VAT rate to use
    for this line.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- tests/ui/acq_receipt_lines/test_acq_receipt_lines_api.py
+++ tests/ui/acq_receipt_lines/test_acq_receipt_lines_api.py
@@ -27,17 +27,27 @@ def test_receipt_lines_properties(acq_receipt_fiction_martigny,
                                   lib_martigny, acq_account_fiction_martigny):
     """Test receipt line properties."""
     acrl1 = acq_receipt_line_1_fiction_martigny
+    acre = acq_receipt_fiction_martigny
     # LIBRARY------------------------------------------------------------------
     assert acrl1.library_pid == acq_receipt_fiction_martigny.library_pid
     # ORGANISATION ------------------------------------------------------------
     assert acrl1.organisation_pid == lib_martigny.organisation_pid
     # ORDER LINE --------------------------------------------------------------
     assert acrl1.order_line_pid == acq_order_line_fiction_martigny.pid
+    acol = acq_order_line_fiction_martigny
+    assert acol.receipt_date.strftime('%Y-%m-%d') == acrl1.get('receipt_date')
+
     # NOTE --------------------------------------------------------------------
     assert acrl1.get_note(AcqReceiptLineNoteType.STAFF)
     # ACQ ACCOUNT -------------------------------------------------------------
     assert acq_account_fiction_martigny.expenditure_amount == (1001.0, 0.0)
 
-    # TEST order_line.reception_date
-    acol = acq_order_line_fiction_martigny
-    assert acol.receipt_date.strftime('%Y-%m-%d') == acrl1.get('receipt_date')
+    # TOTAL AMOUNT ------------------------------------------------------------
+    #   The receipt line total amount has multiple variables : quantity,
+    #   amount, exchange rate and VAT rate
+    #   Starting situation is : qte=1, amount=1000, vat=0, exchange=0
+    assert acrl1.total_amount == 1000
+    acrl1['vat_rate'] = 6.2  # 1000 * 0.062 --> 62
+    assert acrl1.total_amount == 1062
+    acrl1['vat_rate'] = 100  # 1000 * 1.00 --> 1000
+    assert acrl1.total_amount == 2000
