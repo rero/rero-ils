b75459586b7b378107f80564f5d72f3e6e0608b8
Lauren-D <laurent.dubois@itld-solutions.be>
2022-08-24 14:28:23 +0200

    permissions: refactoring vendor policy
    * Refactoring vendor policy access.
    * Adapts unitest.
    
    Co-Authored-by: Lauren-D <laurent.dubois@itld-solutions.be>

--- rero_ils/modules/vendors/permissions.py
+++ rero_ils/modules/vendors/permissions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 # RERO ILS
-# Copyright (C) 2020 RERO
-# Copyright (C) 2020 UCLouvain
+# Copyright (C) 2022 RERO
+# Copyright (C) 2022 UCLouvain
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -17,78 +17,23 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Permissions for vendors."""
+from invenio_access import action_factory
 
-from rero_ils.modules.patrons.api import current_librarian
-from rero_ils.modules.permissions import RecordPermission
+from rero_ils.modules.permissions import AllowedByAction, \
+    AllowedByActionRestrictByOrganisation, RecordPermissionPolicy
+
+search_action = action_factory('vndr-search')
+read_action = action_factory('vndr-read')
+create_action = action_factory('vndr-create')
+update_action = action_factory('vndr-update')
+delete_action = action_factory('vndr-delete')
 
 
-class VendorPermission(RecordPermission):
-    """Vendor permissions."""
+class VendorPermissionPolicy(RecordPermissionPolicy):
+    """Patron Type Permission Policy used by the CRUD operations."""
 
-    @classmethod
-    def list(cls, user, record=None):
-        """List permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # List organisation allowed only for staff members (lib, sys_lib)
-        return bool(current_librarian)
-
-    @classmethod
-    def read(cls, user, record):
-        """Read permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # only staff members (lib, sys_lib) are allowed to read an organisation
-        if not current_librarian:
-            return False
-        # For staff users, they can read only their own organisation.
-        return current_librarian.organisation_pid == record.organisation_pid
-
-    @classmethod
-    def create(cls, user, record=None):
-        """Create permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # only staff members (lib, sys_lib) can create vendors ...
-        if not current_librarian:
-            return False
-        # ... only for its own organisation
-        if record:
-            return current_librarian.organisation_pid == \
-                    record.organisation_pid
-        return True
-
-    @classmethod
-    def update(cls, user, record):
-        """Update permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        if not record:
-            return False
-            # same as create
-        return cls.create(user, record)
-
-    @classmethod
-    def delete(cls, user, record):
-        """Delete permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True if action can be done.
-        """
-        if not record:
-            return False
-            # same as create
-        return cls.create(user, record)
+    can_search = [AllowedByAction(search_action)]
+    can_read = [AllowedByActionRestrictByOrganisation(read_action)]
+    can_create = [AllowedByActionRestrictByOrganisation(create_action)]
+    can_update = [AllowedByActionRestrictByOrganisation(update_action)]
+    can_delete = [AllowedByActionRestrictByOrganisation(delete_action)]
