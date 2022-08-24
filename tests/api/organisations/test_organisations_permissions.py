b8bd864ba64654edc0345bcb8f2e5fc11f8457b4
Lauren-D <laurent.dubois@itld-solutions.be>
2022-08-24 14:28:23 +0200

    permissions: refactoring vendor policy
    * Refactoring vendor policy access.
    * Adapts unitest.
    
    Co-Authored-by: Lauren-D <laurent.dubois@itld-solutions.be>

--- tests/api/organisations/test_organisations_permissions.py
+++ tests/api/organisations/test_organisations_permissions.py
@@ -20,7 +20,7 @@ from flask import current_app, url_for
 from flask_principal import AnonymousIdentity, identity_changed
 from flask_security.utils import login_user
 from invenio_accounts.testutils import login_user_via_session
-from utils import get_json
+from utils import check_permission, get_json
 
 from rero_ils.modules.organisations.permissions import \
     OrganisationPermissionPolicy
@@ -83,18 +83,11 @@ def test_organisation_permissions(patron_martigny,
 
     permission_policy = OrganisationPermissionPolicy
 
-    def check_permission(actions, record):
-        for action_name, action_result in actions.items():
-            result = permission_policy(action_name, record=record).can()
-            assert \
-                result == action_result, \
-                f'{action_name} :: return {result} but should {action_result}'
-
     # Anonymous user
     identity_changed.send(
         current_app._get_current_object(), identity=AnonymousIdentity()
     )
-    check_permission({
+    check_permission(permission_policy, {
         'search': False,
         'read': False,
         'create': False,
@@ -105,7 +98,7 @@ def test_organisation_permissions(patron_martigny,
     # Patron
     #    A simple patron can't operate any operation about Organisation
     login_user(patron_martigny.user)
-    check_permission({
+    check_permission(permission_policy, {
         'search': False,
         'read': False,
         'create': False,
@@ -118,15 +111,15 @@ def test_organisation_permissions(patron_martigny,
     #     - read : only Organisation for its own organisation
     #     - create/update/delete: disallowed
     login_user(librarian_martigny.user)
-    check_permission({'search': True}, None)
-    check_permission({'create': False}, {})
-    check_permission({
+    check_permission(permission_policy, {'search': True}, None)
+    check_permission(permission_policy, {'create': False}, {})
+    check_permission(permission_policy, {
         'read': True,
         'create': False,
         'update': False,
         'delete': False
     }, org_martigny)
-    check_permission({
+    check_permission(permission_policy, {
         'read': False,
         'create': False,
         'update': False,
@@ -138,15 +131,15 @@ def test_organisation_permissions(patron_martigny,
     #     - read/update : only Organisation for its own organisation
     #     - create/delete : always disallowed (only CLI command)
     login_user(system_librarian_martigny.user)
-    check_permission({'search': True}, None)
-    check_permission({'create': False}, {})
-    check_permission({
+    check_permission(permission_policy, {'search': True}, None)
+    check_permission(permission_policy, {'create': False}, {})
+    check_permission(permission_policy, {
         'read': True,
         'create': False,
         'update': True,
         'delete': False
     }, org_martigny)
-    check_permission({
+    check_permission(permission_policy, {
         'read': False,
         'create': False,
         'update': False,
