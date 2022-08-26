029c5263c01b7738db2d73657ee6706193e89f09
Lauren-D <laurent.dubois@itld-solutions.be>
2022-08-26 14:29:36 +0200

    permissions: refactoring item type policy
    * Refactoring item type policy access.
    * Adapts unitest.
    
    Co-Authored-by: Lauren-D <laurent.dubois@itld-solutions.be>

--- rero_ils/modules/item_types/permissions.py
+++ rero_ils/modules/item_types/permissions.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 # RERO ILS
-# Copyright (C) 2020 RERO
-# Copyright (C) 2020 UCLouvain
+# Copyright (C) 2022 RERO
+# Copyright (C) 2022 UCLouvain
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -17,79 +17,23 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Permissions for item types."""
+from invenio_access import action_factory
 
-from rero_ils.modules.patrons.api import current_librarian
-from rero_ils.modules.permissions import RecordPermission
+from rero_ils.modules.permissions import AllowedByAction, \
+    AllowedByActionRestrictByOrganisation, RecordPermissionPolicy
+
+search_action = action_factory('itty-search')
+read_action = action_factory('itty-read')
+create_action = action_factory('itty-create')
+update_action = action_factory('itty-update')
+delete_action = action_factory('itty-delete')
 
 
-class ItemTypePermission(RecordPermission):
-    """Item type permissions."""
+class ItemTypePermissionPolicy(RecordPermissionPolicy):
+    """Patron Type Permission Policy used by the CRUD operations."""
 
-    @classmethod
-    def list(cls, user, record=None):
-        """List permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # Operation allowed only for staff members (lib, sys_lib)
-        return bool(current_librarian)
-
-    @classmethod
-    def read(cls, user, record):
-        """Read permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # Check the user is authenticated and a record exists as param.
-        if not record or not current_librarian:
-            return False
-        # Check if record correspond to user owning organisation and that user
-        # is (at least) a librarian
-        return current_librarian.organisation_pid == record.organisation_pid
-
-    @classmethod
-    def create(cls, user, record=None):
-        """Create permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # only system_librarian can create patron types ...
-        if not current_librarian or not current_librarian.has_full_permissions:
-            return False
-        # ... only for its own organisation
-        if record:
-            return current_librarian.organisation_pid == \
-                   record.organisation_pid
-        return True
-
-    @classmethod
-    def update(cls, user, record):
-        """Update permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        if not record:
-            return False
-        # same as create
-        return cls.create(user, record)
-
-    @classmethod
-    def delete(cls, user, record):
-        """Delete permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True if action can be done.
-        """
-        if not record:
-            return False
-        # same as create
-        return cls.create(user, record)
+    can_search = [AllowedByAction(search_action)]
+    can_read = [AllowedByActionRestrictByOrganisation(read_action)]
+    can_create = [AllowedByActionRestrictByOrganisation(create_action)]
+    can_update = [AllowedByActionRestrictByOrganisation(update_action)]
+    can_delete = [AllowedByActionRestrictByOrganisation(delete_action)]
