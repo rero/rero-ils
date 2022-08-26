3dedf3d377bdce8614ba8ad3dd1e63b1fb356137
Renaud Michotte <renaud.michotte@gmail.com>
2022-08-26 15:34:27 +0200

    permission: acquisition resources policies
    * Refactors Budget permission policies resource.
    * Refactors AcqAccount permission policies resource.
    * Refactors AcqOrder permission policies resource.
    * Refactors AcqOrderLine permission policies resource.
    * Refactors AcqReceipt permission policies resource.
    * Refactors AcqInvoice permission policies resource.
    * Cleans uni-tests to remove unnecessary tests.
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- tests/api/acq_order_lines/test_acq_order_lines_permissions.py
+++ tests/api/acq_order_lines/test_acq_order_lines_permissions.py
@@ -17,105 +17,17 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import mock
-from flask import url_for
-from invenio_accounts.testutils import login_user_via_session
-from utils import get_json
+from flask import current_app
+from flask_principal import AnonymousIdentity, identity_changed
+from flask_security import login_user
+from utils import check_permission
 
-from rero_ils.modules.acq_order_lines.permissions import AcqOrderLinePermission
-
-
-def test_order_lines_permissions_api(client, document, org_martigny,
-                                     vendor2_martigny, lib_sion,
-                                     patron_martigny,
-                                     system_librarian_martigny,
-                                     librarian_martigny,
-                                     acq_order_line_fiction_martigny,
-                                     acq_order_line_fiction_saxon,
-                                     acq_order_line_fiction_sion):
-    """Test order lines permissions api."""
-    acq_oline_permissions_url = url_for(
-        'api_blueprint.permissions',
-        route_name='acq_order_lines'
-    )
-    acq_oline_martigny_permission_url = url_for(
-        'api_blueprint.permissions',
-        route_name='acq_order_lines',
-        record_pid=acq_order_line_fiction_martigny.pid
-    )
-    acq_oline_saxon_permission_url = url_for(
-        'api_blueprint.permissions',
-        route_name='acq_order_lines',
-        record_pid=acq_order_line_fiction_saxon.pid
-    )
-    acq_oline_sion_permission_url = url_for(
-        'api_blueprint.permissions',
-        route_name='acq_order_lines',
-        record_pid=acq_order_line_fiction_sion.pid
-    )
-
-    # Not logged
-    res = client.get(acq_oline_permissions_url)
-    assert res.status_code == 401
-
-    # Logged as patron
-    login_user_via_session(client, patron_martigny.user)
-    res = client.get(acq_oline_permissions_url)
-    assert res.status_code == 403
-
-    # Logged as librarian
-    #   * lib can 'list' and 'read' order lines of its own organisation
-    #   * lib can 'create', 'update', delete only for its library
-    #   * lib can't 'read' acq_account of others organisation.
-    #   * lib can't 'create', 'update', 'delete' order lines for other org/lib
-    login_user_via_session(client, librarian_martigny.user)
-    res = client.get(acq_oline_martigny_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert data['read']['can']
-    assert data['list']['can']
-    assert data['create']['can']
-    assert data['update']['can']
-    assert data['delete']['can']
-
-    res = client.get(acq_oline_saxon_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert not data['read']['can']
-    assert data['list']['can']
-    assert not data['update']['can']
-    assert not data['delete']['can']
-
-    res = client.get(acq_oline_sion_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert not data['read']['can']
-    assert data['list']['can']
-    assert not data['update']['can']
-    assert not data['delete']['can']
-
-    # Logged as system librarian
-    #   * sys_lib can do everything about order lines of its own organisation
-    #   * sys_lib can't do anything about order lines of other organisation
-    login_user_via_session(client, system_librarian_martigny.user)
-    res = client.get(acq_oline_saxon_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert data['read']['can']
-    assert data['list']['can']
-    assert data['create']['can']
-    assert data['update']['can']
-    assert data['delete']['can']
-
-    res = client.get(acq_oline_sion_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert not data['read']['can']
-    assert not data['update']['can']
-    assert not data['delete']['can']
+from rero_ils.modules.acq_order_lines.permissions import \
+    AcqOrderLinePermissionPolicy
 
 
 def test_order_lines_permissions(patron_martigny,
-                                 librarian_martigny,
+                                 librarian_martigny, librarian2_martigny,
                                  system_librarian_martigny,
                                  document, org_martigny, lib_sion,
                                  vendor2_martigny,
@@ -124,57 +36,95 @@ def test_order_lines_permissions(patron_martigny,
                                  acq_order_line_fiction_martigny):
     """Test order lines permissions class."""
 
-    # Anonymous user
-    assert not AcqOrderLinePermission.list(None, {})
-    assert not AcqOrderLinePermission.read(None, {})
-    assert not AcqOrderLinePermission.create(None, {})
-    assert not AcqOrderLinePermission.update(None, {})
-    assert not AcqOrderLinePermission.delete(None, {})
+    # Anonymous user & Patron :: None action allowed
+    identity_changed.send(
+        current_app._get_current_object(), identity=AnonymousIdentity()
+    )
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': False,
+        'read': False,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, {})
+    login_user(patron_martigny.user)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': False,
+        'read': False,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, acq_order_line_fiction_martigny)
 
-    # As non Librarian
-    acq_oline_martigny = acq_order_line_fiction_martigny
-    acq_oline_saxon = acq_order_line_fiction_saxon
-    acq_oline_sion = acq_order_line_fiction_sion
+    # As staff member without any specific access :
+    #   - None action allowed
+    #   - except read record of its own library (pro_read_only)
+    login_user(librarian2_martigny.user)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, acq_order_line_fiction_martigny)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': False,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, acq_order_line_fiction_sion)
 
-    assert not AcqOrderLinePermission.list(None, acq_oline_martigny)
-    assert not AcqOrderLinePermission.read(None, acq_oline_martigny)
-    assert not AcqOrderLinePermission.create(None, acq_oline_martigny)
-    assert not AcqOrderLinePermission.update(None, acq_oline_martigny)
-    assert not AcqOrderLinePermission.delete(None, acq_oline_martigny)
+    # As staff member with "library-administration" role :
+    #   - Search :: everything
+    #   - Read :: record of its own library
+    #   - Create/Update/Delete :: record of its own library
+    login_user(librarian_martigny.user)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': True,
+        'update': True,
+        'delete': True
+    }, acq_order_line_fiction_martigny)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': False,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, acq_order_line_fiction_saxon)
 
-    # As Librarian
+    # As staff member with "full_permissions" role :
+    #   - Search :: everything
+    #   - Read :: record of its own organisation
+    #   - Create/Update/Delete :: record of its own organisation
+    login_user(system_librarian_martigny.user)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': True,
+        'update': True,
+        'delete': True
+    }, acq_order_line_fiction_saxon)
+    check_permission(AcqOrderLinePermissionPolicy, {
+        'search': True,
+        'read': False,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, acq_order_line_fiction_sion)
+
+    # Special case !!! An acquisition order line linked to a closed budget
+    # should be considerate as roll-overed and can't be updated.
     with mock.patch(
-        'rero_ils.modules.permissions.current_librarian',
-        librarian_martigny
+        'rero_ils.modules.acq_order_lines.api.AcqOrderLine.is_active',
+        False
     ):
-        assert AcqOrderLinePermission.list(None, acq_oline_martigny)
-        assert AcqOrderLinePermission.read(None, acq_oline_martigny)
-        assert AcqOrderLinePermission.create(None, acq_oline_martigny)
-        assert AcqOrderLinePermission.update(None, acq_oline_martigny)
-        assert AcqOrderLinePermission.delete(None, acq_oline_martigny)
-
-        assert not AcqOrderLinePermission.read(None, acq_oline_saxon)
-        assert not AcqOrderLinePermission.create(None, acq_oline_saxon)
-        assert not AcqOrderLinePermission.update(None, acq_oline_saxon)
-        assert not AcqOrderLinePermission.delete(None, acq_oline_saxon)
-
-        assert not AcqOrderLinePermission.read(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.create(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.update(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.delete(None, acq_oline_sion)
-
-    # As System-librarian
-    with mock.patch(
-        'rero_ils.modules.permissions.current_librarian',
-        system_librarian_martigny
-    ):
-        assert AcqOrderLinePermission.list(None, acq_oline_saxon)
-        assert AcqOrderLinePermission.read(None, acq_oline_saxon)
-        assert AcqOrderLinePermission.create(None, acq_oline_saxon)
-        assert AcqOrderLinePermission.update(None, acq_oline_saxon)
-        assert AcqOrderLinePermission.delete(None, acq_oline_saxon)
-
-        assert not AcqOrderLinePermission.read(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.create(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.update(None, acq_oline_sion)
-        assert not AcqOrderLinePermission.delete(None, acq_oline_sion)
+        check_permission(AcqOrderLinePermissionPolicy, {
+            'search': True,
+            'read': True,
+            'create': False,
+            'update': False,
+            'delete': False
+        }, acq_order_line_fiction_martigny)
