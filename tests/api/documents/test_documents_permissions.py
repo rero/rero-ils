2b3d4c59f563888f2ec0cd578366d0b7137c7e56
Renaud Michotte <renaud.michotte@gmail.com>
2022-08-30 16:27:35 +0200

    permissions: document/holding/item resource policies refactoring
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- tests/api/documents/test_documents_permissions.py
+++ tests/api/documents/test_documents_permissions.py
@@ -17,76 +17,89 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 import mock
-from flask import url_for
-from invenio_accounts.testutils import login_user_via_session
-from utils import get_json
+from flask import current_app
+from flask_principal import AnonymousIdentity, identity_changed
+from flask_security import login_user
+from utils import check_permission, flush_index
 
-from rero_ils.modules.documents.permissions import DocumentPermission
-
-
-def test_documents_permissions_api(client, document, ebook_1,
-                                   system_librarian_martigny):
-    """Test documents permissions api."""
-    # Logged as system librarian.
-    #   * All operation are allowed for normal document
-    #   * Even a harvested document cannot be delete/edit
-    doc_permission_url = url_for(
-        'api_blueprint.permissions',
-        route_name='documents',
-        record_pid=document.pid
-    )
-    login_user_via_session(client, system_librarian_martigny.user)
-    res = client.get(doc_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert data['read']['can']
-    assert data['list']['can']
-    assert data['create']['can']
-    assert data['update']['can']
-    assert data['delete']['can']
-
-    doc_permission_url = url_for(
-        'api_blueprint.permissions',
-        route_name='documents',
-        record_pid=ebook_1.pid
-    )
-    login_user_via_session(client, system_librarian_martigny.user)
-    res = client.get(doc_permission_url)
-    assert res.status_code == 200
-    data = get_json(res)
-    assert data['read']['can']
-    assert data['list']['can']
-    assert data['create']['can']
-    assert not data['update']['can']
-    assert not data['delete']['can']
+from rero_ils.modules.documents.permissions import DocumentPermissionPolicy
+from rero_ils.modules.patrons.api import PatronsSearch
 
 
-def test_documents_permissions(patron_martigny,
-                               librarian_martigny,
-                               document):
+def test_documents_permissions(
+    patron_martigny, librarian_martigny, document
+):
     """Test documents permissions class."""
+    # Anonymous user & Patron user
+    #  - search/read any document are allowed.
+    #  - create/update/delete operations are disallowed.
+    identity_changed.send(
+        current_app._get_current_object(), identity=AnonymousIdentity()
+    )
+    check_permission(DocumentPermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, None)
+    check_permission(DocumentPermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, document)
+    login_user(patron_martigny.user)
+    check_permission(DocumentPermissionPolicy, {'create': False}, {})
+    check_permission(DocumentPermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, document)
 
-    # Anonymous user
-    assert DocumentPermission.list(None, document)
-    assert DocumentPermission.read(None, document)
-    assert not DocumentPermission.create(None, document)
-    assert not DocumentPermission.update(None, document)
-    assert not DocumentPermission.delete(None, document)
+    # Librarian with specific role
+    #     - search/read: any document
+    #     - create/update/delete: allowed for any document
+    login_user(librarian_martigny.user)
+    check_permission(DocumentPermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': True,
+        'update': True,
+        'delete': True
+    }, document)
 
-    # As non Librarian
-    assert DocumentPermission.list(None, document)
-    assert DocumentPermission.read(None, document)
-    assert not DocumentPermission.create(None, document)
-    assert not DocumentPermission.update(None, document)
-    assert not DocumentPermission.delete(None, document)
+    # Librarian without specific role
+    #   - search/read: any document
+    #   - create/update/delete: disallowed for any document !!
+    original_roles = librarian_martigny.get('roles', [])
+    librarian_martigny['roles'] = ['pro_circulation_manager']
+    librarian_martigny.update(librarian_martigny, dbcommit=True, reindex=True)
+    flush_index(PatronsSearch.Meta.index)
 
-    # As Librarian
-    with mock.patch(
-        'rero_ils.modules.documents.permissions.current_librarian',
-        librarian_martigny
-    ):
-        assert DocumentPermission.list(None, document)
-        assert DocumentPermission.read(None, document)
-        assert DocumentPermission.create(None, document)
-        assert DocumentPermission.update(None, document)
-        assert DocumentPermission.delete(None, document)
+    login_user(librarian_martigny.user)  # to refresh identity !
+    check_permission(DocumentPermissionPolicy, {
+        'search': True,
+        'read': True,
+        'create': False,
+        'update': False,
+        'delete': False
+    }, document)
+
+    # reset the librarian
+    librarian_martigny['roles'] = original_roles
+    librarian_martigny.update(librarian_martigny, dbcommit=True, reindex=True)
+    flush_index(PatronsSearch.Meta.index)
+
+    # Test if the document cannot be edited (harvested documents, ...)
+    with mock.patch('rero_ils.modules.documents.api.Document.can_edit', False):
+        check_permission(DocumentPermissionPolicy, {
+            'search': True,
+            'read': True,
+            'create': False,
+            'update': False,
+            'delete': False
+        }, document)
