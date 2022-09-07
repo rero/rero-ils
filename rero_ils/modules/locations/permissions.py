b4fa0ed6356d44487dd949fa41fab7911b73448a
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2022-09-07 14:15:41 +0200

    permissions: refactoring location policy
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- rero_ils/modules/locations/permissions.py
+++ rero_ils/modules/locations/permissions.py
@@ -17,80 +17,25 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """Permissions for locations."""
+from invenio_access import action_factory
 
-from rero_ils.modules.patrons.api import current_librarian, current_patrons
-from rero_ils.modules.permissions import RecordPermission
+from rero_ils.modules.permissions import AllowedByAction, \
+    AllowedByActionRestrictByManageableLibrary, \
+    AllowedByActionRestrictByOrganisation, RecordPermissionPolicy
+
+# Actions to control location policy
+search_action = action_factory('loc-search')
+read_action = action_factory('loc-read')
+create_action = action_factory('loc-create')
+update_action = action_factory('loc-update')
+delete_action = action_factory('loc-delete')
 
 
-class LocationPermission(RecordPermission):
-    """Location permissions."""
+class LocationPermissionPolicy(RecordPermissionPolicy):
+    """Location Permission Policy used by the CRUD operations."""
 
-    @classmethod
-    def list(cls, user, record=None):
-        """List permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        return bool(current_librarian or current_patrons)
-
-    @classmethod
-    def read(cls, user, record):
-        """Read permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        return bool(current_librarian or current_patrons)
-
-    @classmethod
-    def create(cls, user, record=None):
-        """Create permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # only staff members (sys_lib, lib) can create location
-        if not current_librarian:
-            return False
-        if not record:  # Used to to know if user could create some location
-            return True
-        else:
-            # same as update
-            return cls.update(user, record)
-
-    @classmethod
-    def update(cls, user, record):
-        """Update permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True is action can be done.
-        """
-        # only staff members (lib, sys_lib) can update location
-        # record cannot be null
-        if not current_librarian or not record:
-            return False
-        if current_librarian.organisation_pid == record.organisation_pid:
-            # 'sys_lib' can update all locations
-            if current_librarian.has_full_permissions:
-                return True
-            # 'lib' can only update location linked to its own library
-            else:
-                return current_librarian.library_pids and \
-                       record.library_pid in current_librarian.library_pids
-        return False
-
-    @classmethod
-    def delete(cls, user, record):
-        """Delete permission check.
-
-        :param user: Logged user.
-        :param record: Record to check.
-        :return: True if action can be done.
-        """
-        # same as update
-        return cls.update(user, record)
+    can_search = [AllowedByAction(search_action)]
+    can_read = [AllowedByActionRestrictByOrganisation(read_action)]
+    can_create = [AllowedByActionRestrictByManageableLibrary(create_action)]
+    can_update = [AllowedByActionRestrictByManageableLibrary(update_action)]
+    can_delete = [AllowedByActionRestrictByManageableLibrary(delete_action)]
