0514b97107e3ce1c9ec42998e9d1a287e27a6ef0
Lauren-D <laurent.dubois@itld-solutions.be>
2023-06-26 10:38:26 +0200

    entities: configure operation logs for local entities
    * Uses default operation log extension.
    
    Co-Authored-by: Lauren-D <laurent.dubois@itld-solutions.be>

--- rero_ils/modules/entities/local_entities/jsonresolver.py
+++ rero_ils/modules/entities/local_entities/jsonresolver.py
@@ -25,5 +25,5 @@ from rero_ils.modules.jsonresolver import resolve_json_refs
 
 @jsonresolver.route('/api/local_entities/<pid>', host='bib.rero.ch')
 def local_entities_resolver(pid):
-    """Resolver for acq_account record."""
+    """Resolver for local entity record."""
     return resolve_json_refs('locent', pid)
