533f7470ad80bd0fe07a1bf1d38b5ea647fc97c1
Renaud Michotte <renaud.michotte@gmail.com>
2023-07-04 16:19:18 +0200

    entities: proxy to search local entities.
    Creates an API endpoint to search local entities by category (a category
    could represent multiple ES filters coupling AND/OR/... filters).
    
    Co-Authored-by: Renaud Michotte <renaud.michotte@gmail.com>

--- rero_ils/modules/entities/remote_entities/proxy.py
+++ rero_ils/modules/entities/remote_entities/proxy.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Entity proxies."""
+"""Remote entity proxies."""
 import json
 from urllib.parse import quote_plus
 
