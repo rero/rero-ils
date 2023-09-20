8071350d024cad94bc25c82bfad57cf1a3ba66ef
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2023-09-20 15:43:10 +0200

    statistics: add resource StatConfiguration
    * Adds REST configuration.
    * Adds specific permissions.
    
    Co-Authored-by: Valeria Granata <valeria@chaw.com>
    Co-Authored-by: Renaud Michotte renaud.michotte@gmail.com
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

---
+++ rero_ils/modules/stats_cfg/permissions.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# RERO ILS
+# Copyright (C) 2023 RERO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Permissions for statistics configuration."""
+
+from invenio_access import action_factory
+
+from rero_ils.modules.permissions import AllowedByAction, \
+    AllowedByActionRestrictByOrganisation, RecordPermissionPolicy
+
+# Actions to control statistics configuration policies for CRUD operations
+search_action = action_factory('stat_cfg-search')
+read_action = action_factory('stat_cfg-read')
+create_action = action_factory('stat_cfg-create')
+update_action = action_factory('stat_cfg-update')
+delete_action = action_factory('stat_cfg-delete')
+access_action = action_factory('stat_cfg-access')
+
+
+class StatisticsConfigurationPermissionPolicy(RecordPermissionPolicy):
+    """Statistics configuration permission policy for CRUD operations."""
+
+    can_search = [AllowedByAction(search_action)]
+    can_read = [AllowedByActionRestrictByOrganisation(read_action)]
+    can_create = [AllowedByActionRestrictByOrganisation(create_action)]
+    can_update = [AllowedByActionRestrictByOrganisation(update_action)]
+    can_delete = [AllowedByActionRestrictByOrganisation(delete_action)]
