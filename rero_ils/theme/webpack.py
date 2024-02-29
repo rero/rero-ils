7fcfe808774ff8b7cb28b0fcc74027c2dd060abc
Bertrand Zuchuat <bertrand.zuchuat@rero.ch>
2024-02-29 11:08:00 +0100

    feat: babeltheque
    Co-Authored-by: Bertrand Zuchuat <bertrand.zuchuat@rero.ch>

--- rero_ils/theme/webpack.py
+++ rero_ils/theme/webpack.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # RERO ILS
-# Copyright (C) 2019-2022 RERO
+# Copyright (C) 2019-2024 RERO
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -30,7 +30,8 @@ theme = WebpackBundle(
     'assets',
     entry={
         'global': './scss/rero_ils/styles.scss',
-        'reroils_public': './js/reroils/public.js'
+        'reroils_public': './js/reroils/public.js',
+        'babeltheque': './scss/rero_ils/babeltheque.scss'
     },
     dependencies={
         'popper.js': '1.16.1',
