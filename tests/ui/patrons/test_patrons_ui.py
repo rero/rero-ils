a557424998b4ab91f5a8f12ea407a48b61f57a05
Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>
2024-11-20 08:53:16 +0100

    ui: uses primeng for the frontend
    * Adds missing format alias on the the REST API.
    * Adds placeholder on the contries editor.
    * All document detailed view tabs is done in angular.
    * Removes useless document views.
    * Removes useless document jinja template filters and their corresponding tests.
    * Adds layer to css styles to avoid primeng and bootstrap conflicts.
    
    Co-Authored-by: Johnny Mariéthoz <Johnny.Mariethoz@rero.ch>

--- tests/ui/patrons/test_patrons_ui.py
+++ tests/ui/patrons/test_patrons_ui.py
@@ -22,8 +22,6 @@ from flask import url_for
 from invenio_accounts.testutils import login_user_via_session
 from utils import get_json
 
-from rero_ils.modules.patrons.views import format_currency_filter
-
 
 def test_patrons_logged_user(client, librarian_martigny):
     """Test logged user info API."""
@@ -62,10 +60,3 @@ def test_patrons_logged_user(client, librarian_martigny):
             == librarian_martigny["libraries"][0]["$ref"].rsplit("/", 1)[-1]
         )
         assert data.get("settings").get("language") == "fr"
-
-
-def test_patron_format_currency_filter(app):
-    """Test format currency filter."""
-    assert format_currency_filter(3, "EUR") == "€3.00"
-    assert format_currency_filter(4.5, "CHF") == "CHF4.50"
-    assert format_currency_filter(None, "EUR") is None
