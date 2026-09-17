ffb2ddcd678a09c7147725e3b8d7d54a5668537f
anouillz <anagoncalves7@icloud.com>
2026-09-17 11:51:02 +0200

    fix(requests): hide not received serial issues
    Serial issues that are requested but were not received yet do not
    appear on the request list anymore. They will appear once their status
    becomes "received".
    
    * Exclude items whose issue status is not "received" from the request-
      to-validate item lookup query.
    * Closes #3601.
    
    Co-Authored-by: Ana Gomes <anagoncalves7@icloud.com>

--- rero_ils/modules/loans/api.py
+++ rero_ils/modules/loans/api.py
@@ -9,7 +9,7 @@ from bisect import bisect_right
 from datetime import UTC, datetime, timedelta
 
 import ciso8601
-from elasticsearch_dsl import A
+from elasticsearch_dsl import A, Q
 from flask import current_app
 from flask_babel import gettext as _
 from invenio_circulation.errors import MissingRequiredParameterError
@@ -34,7 +34,7 @@ from rero_ils.modules.circ_policies.api import (
     CircPolicy,
 )
 from rero_ils.modules.errors import NoCirculationActionIsPermitted
-from rero_ils.modules.items.models import ItemStatus
+from rero_ils.modules.items.models import ItemIssueStatus, ItemStatus
 from rero_ils.modules.items.utils import item_pid_to_object
 from rero_ils.modules.libraries.api import LibrariesSearch, Library
 from rero_ils.modules.locations.api import Location, LocationsSearch
@@ -370,6 +370,13 @@ class Loan(IlsRecord):
                     ItemsSearch()
                     .filter("term", pid=pid)
                     .filter("term", status=ItemStatus.ON_SHELF)
+                    # exclude issues that have not been physically received yet
+                    .filter(
+                        Q(
+                            "bool",
+                            must_not=[Q("exists", field="issue") & ~Q("term", issue__status=ItemIssueStatus.RECEIVED)],
+                        )
+                    )
                     .source(includes=fields)
                     .execute()
                 )
